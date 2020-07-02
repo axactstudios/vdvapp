2f4fb7f76323e5814dd99ce91db046aad4e1424e
scorchy <vkumarsaraswat@gmail.com>
2020-07-02 21:23:37 +0530

    Recharge Screen done

--- lib/NavBarPages/Recharge.dart
+++ lib/NavBarPages/Recharge.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:getflutter/components/appbar/gf_appbar.dart';
 
@@ -9,10 +10,191 @@ class RechargePage extends StatefulWidget {
 }
 
 class _RechargePageState extends State<RechargePage> {
+  String amount = "", stb = "";
+  bool saveAttempt = false;
+  final listOfPlans = ["Regular", "Family", "Premium HD"];
+//  (Rs.200/month, 170 Channels)
+//  (Rs.300/month, 300 Channels)
+//  (Rs.500/month, 400 SD Channels, 30 HD Channels)
+  String DropdownValue = 'Regular';
+
+  final formKey = GlobalKey<FormState>();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF573280),
+        title: Text(
+          "VDV",
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+              fontFamily: 'nunito'),
+        ),
+      ),
+      body: SafeArea(
+        child: ListView(
+          shrinkWrap: true,
+          children: [
+            Form(
+              key: formKey,
+              child: Padding(
+                padding: EdgeInsets.all(50),
+                child: Column(
+                  children: [
+                    Text(
+                      'Enter Recharge Details',
+                      style: TextStyle(
+                          color: Color(0xFF573280),
+                          fontSize: 30,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 40.0,
+                    ),
+                    TextFormField(
+                      onChanged: (textValue) {
+                        setState(() {
+                          stb = textValue;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF573280),
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF573280),
+                          ),
+                        ),
+                        hintText: 'STB No.',
+                        hintStyle: TextStyle(
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.black, fontSize: 16.0),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please enter STB no.';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    DropdownButtonFormField(
+                      value: DropdownValue,
+                      icon: Icon(Icons.arrow_downward),
+                      decoration: InputDecoration(
+                        labelText: "Plans",
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF573280),
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                      items: listOfPlans.map((String value) {
+                        return new DropdownMenuItem<String>(
+                          value: value,
+                          child: new Text(
+                            value,
+                            overflow: TextOverflow.fade,
+                          ),
+                        );
+                      }).toList(),
+                      onChanged: (String newValue) {
+                        setState(() {
+                          DropdownValue = newValue;
+                        });
+                      },
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please Select Plan';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    TextFormField(
+                      onChanged: (textValue) {
+                        setState(() {
+                          amount = textValue;
+                        });
+                      },
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF573280),
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        focusedBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(
+                            color: Color(0xFF573280),
+                          ),
+                        ),
+                        hintText: 'Enter Amount',
+                        hintStyle: TextStyle(
+                          color: Colors.black.withOpacity(0.3),
+                        ),
+                      ),
+                      style: TextStyle(color: Colors.black, fontSize: 16.0),
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return 'Please Select Plan';
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(
+                      height: 20.0,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        setState(() {
+                          saveAttempt = true;
+                        });
+                        if (formKey.currentState.validate()) {
+                          formKey.currentState.save();
+                        }
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(
+                            vertical: 16.0, horizontal: 54.0),
+                        decoration: BoxDecoration(
+                            color: Color(0xFF573280),
+                            borderRadius: BorderRadius.circular(15.0)),
+                        child: Text(
+                          'Recharge',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 18.0,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-  // ignore: non_constant_identifier_names
-
 }
